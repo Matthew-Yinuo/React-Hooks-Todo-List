cc1d1a1398166509628755e7db79c93b0f5b8a6a
Matthew Hoth <matthewhoth@gmail.com>
2018-11-18 18:25:21 +0800

    Make the import field for the todo list look a little nicer.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Form from '../components/Form'
+import Form from '../components/form'
 import './index.css'
 import { setConfig } from 'react-hot-loader'
 
